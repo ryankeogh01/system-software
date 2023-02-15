29592b3480963071c80b28aca69333d3f5730620
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2023-02-15 17:58:58 +0000

    added more code

--- pointers-refresher/pointer-structs.c
+++ pointers-refresher/pointer-structs.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// we define a struct that contains three int values: year, month and day. 
+// We use typedef to name our new struct type date.
 typedef struct {
   int year;
   int month;
@@ -9,10 +11,15 @@ typedef struct {
 
 int main(void) {
 
+  // we declare a new variable today to be a pointer to date. 
   date *today;
+  // we use malloc() to allocate a block of memory (on the heap) to store one date struct.
   today = (date*)malloc(sizeof(date));
 
   // the explicit way of accessing fields of our struct
+  // this uses explicit pointer syntax. S
+  // for example the expression (*today).day means, dereference the today pointer and then access the day field 
+  // of the thing you find there (which will be a date struct).
   (*today).day = 15;
   (*today).month = 6;
   (*today).year = 2022;
