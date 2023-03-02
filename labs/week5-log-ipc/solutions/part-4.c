075a27b0b7ea1a25e3283c39941d93d9e97f9df4
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2023-03-02 19:39:21 +0000

    added one solution

---
+++ labs/week5-log-ipc/solutions/part-4.c
@@ -0,0 +1,108 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void exec1();
+void exec2();
+void exec3();
+
+int pid;
+int pipe1[2];
+int pipe2[2];
+
+void main() {
+  // ps aux | awk { print $1 } | sort -u	
+
+  // create pipe1
+  if (pipe(pipe1) == -1) {
+    perror("bad pipe1");
+    exit(1);
+  }
+
+  // fork (ps aux)
+  if ((pid = fork()) == -1) {
+    perror("bad fork1");
+    exit(1);
+  } else if (pid == 0) {
+    // stdin --> ps --> pipe1
+    exec1();
+  }
+  // parent
+
+  // create pipe2
+  if (pipe(pipe2) == -1) {
+    perror("bad pipe2");
+    exit(1);
+  }
+
+  // fork (awk { print $1 })
+  if ((pid = fork()) == -1) {
+    perror("bad fork2");
+    exit(1);
+  } else if (pid == 0) {
+    // pipe1 --> grep --> pipe2
+    exec2();
+  }
+  // parent
+
+  // close unused fds
+  close(pipe1[0]);
+  close(pipe1[1]);
+
+  // fork (grep sbin)
+  if ((pid = fork()) == -1) {
+    perror("bad fork3");
+    exit(1);
+  } else if (pid == 0) {
+    // pipe2 --> grep --> stdout
+    exec3();
+  }
+  // parent
+
+
+}
+
+
+void exec1() {
+  // input from stdin (already done)
+  // output to pipe1
+  dup2(pipe1[1], 1);
+  // close fds
+  close(pipe1[0]);
+  close(pipe1[1]);
+  // exec
+  execlp("ls", "ls", "-al", NULL);
+  // exec didn't work, exit
+  perror("bad exec ps");
+  exit(1);
+}
+
+void exec2() {
+  // input from pipe1
+  dup2(pipe1[0], 0);
+  // output to pipe2
+  dup2(pipe2[1], 1);
+  // close fds
+  close(pipe1[0]);
+  close(pipe1[1]);
+  close(pipe2[0]);
+  close(pipe2[1]);
+  // exec
+  execlp("awk", "awk", "{ print $3 }", NULL);
+  // exec didn't work, exit
+  perror("bad exec awk");
+  exit(1);
+}
+
+void exec3() {
+  // input from pipe2
+  dup2(pipe2[0], 0);
+  // output to stdout (already done)
+  // close fds
+  close(pipe2[0]);
+  close(pipe2[1]);
+  // exec
+  execlp("sort", "sort", "-u", NULL);
+  // exec didn't work, exit
+  perror("bad exec sort");
+  _exit(1);
+}
